402671b8e4e2fd2b7c7a81ab5c3242a3a7d5ca35
bzayn02 <nagarkotibezay@gmail.com>
2023-06-04 15:32:36 +1000

    updated routers get,post and delete

--- src/routers/taskRouter.js
+++ src/routers/taskRouter.js
@@ -1,7 +1,33 @@
 import express from 'express';
 const router = express.Router();
 
-const fakeDB = [];
+let fakeDB = [
+  {
+    task: 'Coding',
+    hr: '33',
+    _id: 'asdf',
+    type: 'entry',
+  },
+  {
+    task: 'Reading',
+    hr: '33',
+    _id: 'asdfg',
+    type: 'entry',
+  },
+  {
+    task: 'Cooking',
+    hr: '33',
+    _id: 'asdfga',
+    type: 'entry',
+  },
+  {
+    task: 'Youtube',
+    hr: '33',
+    _id: 'asdfgh',
+    type: 'entry',
+  },
+];
+
 // Read data from database and return to the client
 router.get('/', (req, res) => {
   res.json({
@@ -13,7 +39,6 @@ router.get('/', (req, res) => {
 // Receive data from client and create new record in database
 router.post('/', (req, res) => {
   console.log('got hit');
-  console.log(req.body);
   fakeDB.push(req.body);
   res.json({
     message: 'New task has been added successfully.',
@@ -22,6 +47,8 @@ router.post('/', (req, res) => {
 
 // Update record in database based on the info received
 router.put('/', (req, res) => {
+  const { _id } = req.body;
+  console.log(_id);
   res.json({
     message: 'To Do Put Method',
   });
@@ -29,8 +56,16 @@ router.put('/', (req, res) => {
 
 // Delete one or many records from  data database based on info received
 router.delete('/', (req, res) => {
+  const { _id } = req.body;
+  console.log(_id);
+
+  // deleting data from fakedb
+
+  fakeDB = fakeDB.filter((item) => item._id !== _id);
+
   res.json({
-    message: 'To Do Delete Method',
+    message: 'The task is deleted successfully.',
+    newData: fakeDB,
   });
 });
 
