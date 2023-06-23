9ba778a491ef0376d66ae0cf37064f2e9426f761
bzayn02 <nagarkotibezay@gmail.com>
2023-06-24 09:20:15 +1000

    local db

--- src/config/mongoDB.js
+++ src/config/mongoDB.js
@@ -2,9 +2,7 @@ import mongoose from 'mongoose';
 
 export const mongoConnect = async () => {
   try {
-    const con = await mongoose.connect(
-      'mongodb+srv://nagarkotibezay02:yKwPwgWY9PzRkCQb@cluster0.or26tsp.mongodb.net/'
-    );
+    const con = await mongoose.connect('mongodb://localhost:27017/tasklists');
     // console.log(con);
     con && console.log('Mongo database is connected!');
   } catch (error) {
